784bc8c7cbfb78b49a9afc14408cbe78262f4464
Dave Floyd <teamkde@gmail.com>
2023-08-19 07:28:47 -0500

    Adding script used to setup Ubuntu and ROS Noetic

--- os-setup.sh
+++ os-setup.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
-apt update -y
-apt upgrade -y
-sudo apt install curl -y  # if you haven't already installed curl
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-apt update -y
-apt install ros-noetic-ros-base -y
-apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential -y
-apt install python3-rosdep -y
-sudo apt-get install python3-catkin-tools
-echo /home/ros/dingo # Add rest of path hereß
+#############################################################################
+# This script sets up ROS Noetic on Ubuntu 20.04
+#############################################################################
+if [[ `id -u` -ne 0 ]]; then 
+	echo "Please run as root"; 
+    exit 1; 
+else    
+    apt update -y
+    apt upgrade -y
+    apt install ubuntu-desktop curl -y
+    apt update -y
+    sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+    curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+    apt update -y
+    apt install ros-noetic-desktop-full -y
+    echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+    apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential -y
+    apt install python3-rosdep -y
+    apt install git -y
+    apt install python3-catkin-tools python3-osrf-pycommon gimp inkscape freecad kicad -y
+    reboot
+fi
